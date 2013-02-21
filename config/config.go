0bc60518eed92ae46d9771fae0513747909366f6
Chris Sexton <chrissexton@users.noreply.github.com>
2013-02-20 22:24:51 -0500

    Adding twitter plugin (non-functional)

--- config/config.go
+++ config/config.go
@@ -7,21 +7,25 @@ import "io/ioutil"
 // Config stores any system-wide startup information that cannot be easily configured via
 // the database
 type Config struct {
-	DbName             string
-	DbServer           string
-	Channels           []string
-	MainChannel        string
-	Plugins            []string
-	Nick, Server, Pass string
-	Version            string
-	CommandChar        string
-	QuoteChance        float64
-	QuoteTime          int
-	LogLength          int
-	Admins             []string
-	UntappdToken       string
-	UntappdFreq        int
-	WelcomeMsgs        []string
+	DbName                string
+	DbServer              string
+	Channels              []string
+	MainChannel           string
+	Plugins               []string
+	Nick, Server, Pass    string
+	Version               string
+	CommandChar           string
+	QuoteChance           float64
+	QuoteTime             int
+	LogLength             int
+	Admins                []string
+	UntappdToken          string
+	UntappdFreq           int
+	WelcomeMsgs           []string
+	TwitterConsumerKey    string
+	TwitterConsumerSecret string
+	TwitterUserKey        string
+	TwitterUserSecret     string
 }
 
 // Readconfig loads the config data out of a JSON file located in cfile
