cfbe0dc4bc6ce9d0f2e1892a7462b47104bf9a8d
Chris Sexton <chrissexton@users.noreply.github.com>
2013-08-31 23:41:15 -0400

    Small log fix

--- plugins/remember.go
+++ plugins/remember.go
@@ -68,8 +68,6 @@ func (p *RememberPlugin) Message(message bot.Message) bool {
 			for i := len(p.Log[message.Channel]) - 1; i >= 0; i-- {
 				entry := p.Log[message.Channel][i]
 
-				log.Println("Test entry:", entry.User.Name, entry.Body)
-
 				if strings.ToLower(entry.User.Name) == strings.ToLower(nick) &&
 					strings.Contains(
 						strings.ToLower(entry.Body),
