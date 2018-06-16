dc345a5c6a252661d49026a4ca7c52fbe65f30aa
skkiesel <skiesel@cs.unh.edu>
2018-06-16 17:19:58 -0400

    This might be entertaining on occasion

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 	"github.com/velour/catbase/plugins/babbler"
 	"github.com/velour/catbase/plugins/beers"
 	"github.com/velour/catbase/plugins/capturetheflag"
+	"github.com/velour/catbase/plugins/couldashouldawoulda"
 	"github.com/velour/catbase/plugins/counter"
 	"github.com/velour/catbase/plugins/dice"
 	"github.com/velour/catbase/plugins/emojifyme"
@@ -71,6 +72,7 @@ func main() {
 	b.AddHandler("twitch", twitch.New(b))
 	b.AddHandler("inventory", inventory.New(b))
 	b.AddHandler("capturetheflag", capturetheflag.New(b))
+	b.AddHandler("couldashouldawoulda", couldashouldawoulda.New(b))
 	// catches anything left, will always return true
 	b.AddHandler("factoid", fact.New(b))
 
