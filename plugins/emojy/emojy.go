e8c7e1e07e3e124e5494f4ed0b50e5f5ec5f04c1
Chris Sexton <3216719+chrissexton@users.noreply.github.com>
2022-06-06 10:20:50 -0400

    emojy: make the web page nicer
    - show custom emojy
    - categorize by source type
    - record emojy used in messages

--- plugins/emojy/emojy.go
+++ plugins/emojy/emojy.go
@@ -1,11 +1,15 @@
 package emojy
 
 import (
+	"github.com/forPelevin/gomoji"
 	"github.com/jmoiron/sqlx"
 	"github.com/rs/zerolog/log"
 	"github.com/velour/catbase/bot"
 	"github.com/velour/catbase/config"
+	"os"
+	"path"
 	"regexp"
+	"strings"
 	"time"
 )
 
@@ -39,6 +43,18 @@ func (p *EmojyPlugin) setupDB() {
 func (p *EmojyPlugin) register() {
 	ht := bot.HandlerTable{
 		{
+			Kind: bot.Message, IsCmd: false,
+			Regex: regexp.MustCompile(`.*`),
+			Handler: func(request bot.Request) bool {
+				r := regexp.MustCompile(`:[a-zA-Z0-9_-]+:`)
+				for _, match := range r.FindAllString(request.Msg.Body, -1) {
+					log.Debug().Msgf("Emojy detected: %s", match)
+					p.recordReaction(match)
+				}
+				return false
+			},
+		},
+		{
 			Kind: bot.Reaction, IsCmd: false,
 			Regex: regexp.MustCompile(`.*`),
 			Handler: func(request bot.Request) bool {
@@ -68,15 +84,48 @@ type EmojyEntry struct {
 
 type EmojyCount struct {
 	Emojy string `json:"emojy"`
+	URL   string `json:"url"`
 	Count int    `json:"count"`
 }
 
-func (p *EmojyPlugin) allCounts() ([]EmojyCount, error) {
+func (p *EmojyPlugin) allCounts() (map[string][]EmojyCount, error) {
+	out := map[string][]EmojyCount{}
 	q := `select emojy, count(observed) as count from emojyLog group by emojy order by count desc`
 	result := []EmojyCount{}
 	err := p.db.Select(&result, q)
 	if err != nil {
 		return nil, err
 	}
-	return result, nil
+	for _, e := range result {
+		if isEmoji(e.Emojy) {
+
+			out["emoji"] = append(out["emoji"], e)
+		} else if ok, fname, _ := p.isKnownEmojy(e.Emojy); ok {
+			e.URL = fname
+			out["emojy"] = append(out["emojy"], e)
+		} else {
+			out["unknown"] = append(out["unknown"], e)
+		}
+	}
+	return out, nil
+}
+
+func (p *EmojyPlugin) isKnownEmojy(name string) (bool, string, error) {
+	emojyPath := p.c.Get("emojy.path", "emojy")
+	baseURL := p.c.Get("emojy.baseURL", "/emojy/file")
+	entries, err := os.ReadDir(emojyPath)
+	if err != nil {
+		return false, "", err
+	}
+	for _, e := range entries {
+		if !e.IsDir() && strings.HasPrefix(e.Name(), name) {
+			url := path.Join(baseURL, e.Name())
+			return true, url, nil
+		}
+	}
+	return false, "", nil
+}
+
+func isEmoji(in string) bool {
+	return gomoji.ContainsEmoji(in)
 }
