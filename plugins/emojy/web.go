a18a17da94bf445cc01ad2902efe7215198742d3
Chris Sexton <3216719+chrissexton@users.noreply.github.com>
2022-06-06 10:20:50 -0400

    emojy: make the web page nicer
    - show custom emojy
    - categorize by source type
    - record emojy used in messages

--- plugins/emojy/web.go
+++ plugins/emojy/web.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"github.com/go-chi/chi/v5"
 	"github.com/rs/zerolog/log"
+	"io/ioutil"
 	"net/http"
+	"path"
 )
 
 //go:embed *.html
@@ -14,6 +16,7 @@ var embeddedFS embed.FS
 func (p *EmojyPlugin) registerWeb() {
 	r := chi.NewRouter()
 	r.HandleFunc("/all", p.handleAll)
+	r.HandleFunc("/file/{name}", p.handleEmojy)
 	r.HandleFunc("/", p.handleIndex)
 	p.b.RegisterWebName(r, "/emojy", "Emojys")
 }
@@ -33,4 +36,16 @@ func (p *EmojyPlugin) handleAll(w http.ResponseWriter, r *http.Request) {
 	}
 	out, _ := json.Marshal(emojy)
 	w.Write(out)
+}
+
+func (p *EmojyPlugin) handleEmojy(w http.ResponseWriter, r *http.Request) {
+	fname := chi.URLParam(r, "name")
+	emojyPath := p.c.Get("emojy.path", "emojy")
+	contents, err := ioutil.ReadFile(path.Join(emojyPath, fname))
+	if err != nil {
+		w.WriteHeader(404)
+		out, _ := json.Marshal(struct{ err error }{err})
+		w.Write(out)
+	}
+	w.Write(contents)
 }
